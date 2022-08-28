c0365c787ffc0cbd5bd5f8e51bf297fbfabdd136
bisdak <krismar.ole@gmail.com>
2022-08-28 14:41:55 +0800

    update test and handling db errors

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	db "github.com/bisdak/simple-bank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -28,6 +29,13 @@ func (server *Server) createAccount(c *gin.Context) {
 
 	account, err := server.store.CreateAccount(c, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				c.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		c.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
